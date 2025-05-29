6b97c1031091aff78c67f740e1c629792aa07616
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-05-29 18:48:12 +0200

    lifetime

--- js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
@@ -20,3 +20,26 @@ Hint: You can combine text and variables using the `+` operator.
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
 console.log("Your current age is: " + currentAge);
+const totalDaysLived = currentAge * 365;
+console.log("The total number of days you lived: " + totalDaysLived);
+const estRemDays = 80 * 365 - totalDaysLived;
+console.log(
+  "Your estimated number of remaining days, assuming an average life span of 80 years: " +
+    estRemDays
+);
+const percLifeLived = (totalDaysLived / (365 * 80)) * 100;
+console.log(
+  "The percentage of life you already lived, in relation to the expected life span: " +
+    percLifeLived +
+    " %"
+);
+const daysSleeping = (totalDaysLived * 8) / 24;
+console.log(
+  "The cumulative number of days you have spent sleeping, assuming an average of 8 hours of sleep per day: " +
+    daysSleeping
+);
+let daysSleeping2 = daysSleeping.toFixed(2);
+console.log(
+  "The cumulative number of days you have spent sleeping, assuming an average of 8 hours of sleep per day: " +
+    daysSleeping2
+);

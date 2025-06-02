255ce3fbb9e4b11f510af9c254a377ab2fac6d85
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-02 12:36:11 +0200

    objects

--- js-dom-and-events/js-dom-and-events_survey-error-fixing/js/index.js
+++ js-dom-and-events/js-dom-and-events_survey-error-fixing/js/index.js
@@ -3,7 +3,12 @@ console.clear();
 // ------------------ 1 & 2 -----------------
 
 // 1. This code selects the "next" button using an ID. This is bad practice. Can you find a better solution?
-const nextButton = document.querySelector("#next");
+// const nextButton = document.querySelector("#next");
+// nextButton.addEventListener("click", () => {
+//   console.log("Go to next question");
+// });
+
+const nextButton = document.querySelector('[data-js="next-button"]');
 nextButton.addEventListener("click", () => {
   console.log("Go to next question");
 });

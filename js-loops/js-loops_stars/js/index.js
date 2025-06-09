61d87a841c407cac0397e7d3dbffe281c6c3fe49
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-09 23:33:23 +0200

    stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -8,7 +8,46 @@ function renderStars() {
 
   // --v-- write or modify code below this line --v--
 
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    img.src = "assets/star-empty.svg";
+    starContainer.append(img);
+  }
   // --^-- write or modify code above this line --^--
+}
+
+renderStars();
+
+function renderStars(filledStars) {
+  starContainer.innerHTML = "";
+
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    if (i <= filledStars) {
+      img.src = "assets/star-filled.svg";
+    } else {
+      img.src = "assets/star-empty.svg";
+    }
+    starContainer.append(img);
+  }
+}
+
+renderStars(2);
+
+function renderStars(filledStars = 0) {
+  starContainer.innerHTML = "";
+
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    img.src =
+      i <= filledStars ? "assets/star-filled.svg" : "assets/star-empty.svg";
+
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+
+    starContainer.append(img);
+  }
 }
 
 renderStars();

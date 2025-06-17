806fb50bf269728fbc25bd956fb0c4e647162222
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-17 12:39:36 +0200

    attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return (
+    <article className="article">
+      Replace me with an article
+      <h2 className="article__title"> blabla</h2>
+      <label htmlFor="example">Input:</label>
+      <input id="example" type="text" />
+      <a
+        className="article__link"
+        href="https://github.com/spiced-academy/turbinado-web-25/blob/main/sessions/react-basics/react-basics.md"
+      >
+        React Basics
+      </a>
+    </article>
+  );
 }

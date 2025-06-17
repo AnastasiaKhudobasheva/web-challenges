678cca46a3e499cdc49fb3b521e33af5ed058d44
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-17 16:05:05 +0200

    greeting

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,19 @@
+// Define the Greeting Component (we destructure the `name` prop):
+
+function Greeting({ name }) {
+  if (name === "Coach") {
+    return <h1>Hello, lovely Coach!</h1>;
+  }
+
+  return <h1>Hello, {name}!</h1>;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Greeting name="Klaus" />
+      <Greeting name="Coach" />
+      <Greeting name="Joseph" />
+    </h1>
+  );
 }

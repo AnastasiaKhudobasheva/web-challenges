158135b0ccd9b9b406cb9cd6e632e6aee02afc4d
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-07-09 11:43:46 +0200

    random

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,17 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  // Generate random values:
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+
+  // Build the response string:
+  const message = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+  // Send response:
+  response.end(message);
+});
